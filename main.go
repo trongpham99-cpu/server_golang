0a894f706456c610ee0c7f937acbb4aaf3778356
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2024-11-23 10:12:57 +0700

    update file response

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "ping pong, hello world",
 		})
 	})
 	r.Run(":4000")
